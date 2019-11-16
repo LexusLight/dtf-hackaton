90f0adaa6d9272140e110f8eea4d8228b3076ce2
Kirill Kudryavtsev <deissh@yandex.ru>
2019-11-16 12:44:24 +0300

    feat: add controller

---
+++ Assets/Scripts/Steam/SteamManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections;
+using Steamworks;
+using UnityEngine;
+
+public class SteamManager : MonoBehaviour
+{
+    private static SteamManager _instance = null; // Экземпляр менеджера
+
+    void Awake()
+    {
+        if (_instance == null) _instance = this;
+        else if (_instance == this) Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        InitializeManager();
+    }
+    
+    void InitializeManager()
+    {
+        Debug.Log("Steam: init");
+        if (!SteamAPI.Init()) return;
+        
+        var name = SteamFriends.GetPersonaName();
+        Debug.Log(name);
+    }
+}
