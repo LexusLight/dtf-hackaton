e08991cfe938a9ca79105dc162fa1943f29ddb09
lexuslight <kostyaelizarov@mail.ru>
2020-09-14 14:14:57 +0300

    уровень + массив текста

---
+++ Magic/Assets/5 Scripts/Menu/Texts/Texts.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Texts : MonoBehaviour
+{
+    public static bool Language;
+
+    public static string[] Ru = {
+        "Внимание!", //0
+        "",//1
+        "Данная игра предназначена либо для гениев, либо для сумасшедших.",//2
+        "Приятного времяпровождения)",//3
+        "",
+    };
+
+    public static string[] Eng = {
+        "Warning!",//0
+        "",//1
+        "This game is intended for geniuses or madmen.",//2
+        "Have good time!",//3
+        "",
+    };
+
+}
