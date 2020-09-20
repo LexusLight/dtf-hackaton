9f5a8c572ee498646442d6604c8809724593d135
lexuslight <kostyaelizarov@mail.ru>
2020-09-20 21:52:39 +0300

    Детали вступления

---
+++ Magic/Assets/5 Scripts/Menu/Texts/InputText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputText : MonoBehaviour
+{
+    public int string_num = 0;
+    public string str;
+    Text text;
+
+    void Awake()
+    {
+        text = gameObject.GetComponent<Text>();
+        str = (Texts.Language == 1) ? Texts.Ru[string_num] : Texts.Eng[string_num];
+        text.text = str;
+    }
+}
--- Magic/Assets/5 Scripts/Menu/Texts/Texts.cs
+++ Magic/Assets/5 Scripts/Menu/Texts/Texts.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class Texts : MonoBehaviour
 {
-    public static bool Language;
+    public static int Language = 0;
 
     public static string[] Ru = {
         "Внимание!", //0
         "",//1
         "Данная игра предназначена либо для гениев, либо для сумасшедших.",//2
         "Приятного времяпровождения)",//3
+        "Когда-то давно....",
+        "Королевство интернетов процветало...",
+        "Но внезапно...",
+        "Его захватила",
+        "Смешная жёлтая собака!",
+        "Именно она... ",
         "",
+        "Ах-да! Собака!",
+
+
+
     };
 
     public static string[] Eng = {
