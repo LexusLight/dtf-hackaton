2261fed2ed68b9d5e3c72528ddbe902f8dbe9914
lexuslight <kostyaelizarov@mail.ru>
2020-09-21 22:34:39 +0300

    prescene+director

--- Magic/Assets/5 Scripts/Menu/Texts/InputText.cs
+++ Magic/Assets/5 Scripts/Menu/Texts/InputText.cs
@@ -7,12 +7,37 @@ public class InputText : MonoBehaviour
 {
     public int string_num = 0;
     public string str;
+    public bool timer = false;
     Text text;
 
     void Awake()
     {
         text = gameObject.GetComponent<Text>();
         str = (Texts.Language == 1) ? Texts.Ru[string_num] : Texts.Eng[string_num];
-        text.text = str;
+        if(!timer) text.text = str;
+    }
+
+    public void OnEnable()
+    {
+        if (timer)
+        {
+            StartCoroutine(InputWithTime(str));
+        }
+    }
+
+    public void OnDisable()
+    {
+        text.text = "";
+    }
+
+    IEnumerator InputWithTime(string str)
+    {
+        text.text = "";
+        for (int i=0; i<str.Length; i++)
+        {
+            yield return new WaitForSeconds(0.05f);
+            text.text = text.text + str[i];
+        }
+        
     }
 }
