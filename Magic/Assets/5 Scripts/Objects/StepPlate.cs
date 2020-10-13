73d2b48b189cf13156e8c0c6e2efd28dda9fc0f4
Hideo Kojima <kostyaelizarov@mail.ru>
2020-10-13 23:59:21 +0300

    22 lvl

--- Magic/Assets/5 Scripts/Objects/StepPlate.cs
+++ Magic/Assets/5 Scripts/Objects/StepPlate.cs
@@ -10,11 +10,19 @@ public class StepPlate : MonoBehaviour
     private byte count = 0;
     private Animator anim;
     private BoxCollider2D col;
+    public bool deactivate = false;
 
     private void Awake()
     {
         anim = gameObject.GetComponent<Animator>();
         col = gameObject.GetComponent<BoxCollider2D>();
+        if (deactivate)
+            foreach (GameObject spike in Spikes)
+            {
+                spike.GetComponent<Animator>().SetBool("Active", false);
+                //spike.GetComponent<Collider2D>().isTrigger = true;
+                spike.GetComponent<Collider2D>().enabled = false;
+            }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
