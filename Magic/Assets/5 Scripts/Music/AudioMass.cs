139608bad8d19d5a97fab8420f2d42b7475e0f4b
Hideo Kojima <kostyaelizarov@mail.ru>
2020-10-19 23:57:58 +0300

    Music

---
+++ Magic/Assets/5 Scripts/Music/AudioMass.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class AudioMass : MonoBehaviour
+{
+    //Просто массив, указывать через интерфейс в сцене 0menu
+    [SerializeField] public AudioClip[] Audiomass;
+    //1 
+    [SerializeField] public AudioClip[] Soundmass;
+
+    [SerializeField] public AudioClip[] Voicemass;
+}
