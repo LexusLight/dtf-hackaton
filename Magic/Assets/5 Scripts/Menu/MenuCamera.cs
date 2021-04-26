004f099151e38034ea7a09078767931daef0eaa0
lexuslight <kostyaelizarov@mail.ru>
2021-04-26 23:06:23 +0300

    Фикс камеры

--- Magic/Assets/5 Scripts/Menu/MenuCamera.cs
+++ Magic/Assets/5 Scripts/Menu/MenuCamera.cs
@@ -9,15 +9,15 @@ public class MenuCamera : MonoBehaviour
     public static float pointX = 0;
     public static float pointY = 0;
 
-    // Update is called once per frame
-    void Update()
+    // Update is called once per frame //Переписать
+    void Update() 
     {
             Camera camera1 = GetComponent<Camera>();
-            Vector3 point = camera1.WorldToViewportPoint(new Vector3(pointX, pointY, -10));
-            Vector3 delta = new Vector3(Mathf.Ceil(pointX), Mathf.Ceil(pointY), 0) - camera1.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 10)); //(new Vector3(0.5, 0.5, point.z));
+            Vector3 point = camera1.WorldToViewportPoint(new Vector3(Mathf.Ceil(pointX), Mathf.Ceil(pointY), 0));
+            Vector3 delta = new Vector3(Mathf.Ceil(pointX), Mathf.Ceil(pointY), 0) - point; //(new Vector3(0.5, 0.5, point.z));
             Vector3 destination = transform.position + delta;
+        
 
-
-            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+            transform.position = Vector3.SmoothDamp(transform.position, delta, ref velocity, dampTime);
     }
 }
